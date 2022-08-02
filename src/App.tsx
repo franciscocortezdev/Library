528189e451048d6622989c1810c27da0319ea67a
Francisco Cortez <franciscocortezdev@gmail.com>
2022-08-01 19:16:43 -0600

    Pages and routes added

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,23 @@
 import './App.css';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+import Home from './Pages/Home';
+import NewBook from './Pages/NewBook';
+import ViewBook from './Pages/ViewBook';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
+    <div>
+      <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/create" element={<NewBook/>} />
+        <Route path="/view/:IDbook" element={<ViewBook/>} />
+      </Routes>
+      </BrowserRouter>
     </div>
   );
 }
