2a5a4fe2451f912f83cfc0216f192b96c2674d20
Francisco Cortez <franciscocortezdev@gmail.com>
2022-08-02 16:51:52 -0600

    NewBook page

--- src/Pages/NewBook.tsx
+++ src/Pages/NewBook.tsx
@@ -1,6 +1,134 @@
+import React, { useState } from 'react'
+
+type NewBookInfo = {
+  id: string
+  title: string,
+  author: string,
+  cover: string,
+  intro: string,
+  completed: boolean,
+  review: string
+}
 
 export default function NewBook () {
+  const [title, setTitle] = useState<string>('')
+  const [author, setAuthor] = useState<string>('')
+  const [cover, setCover] = useState<string>('')
+  const [intro, setIntro] = useState<string>('')
+  const [completed, setCompleted] = useState<boolean>(false)
+  const [review, setReview] = useState<string>('')
+
+  const handleChange = (e: React.FormEvent<HTMLInputElement>) => {
+    const name = e.currentTarget.name
+    const value = e.currentTarget.value
+    switch (name) {
+      case 'title':
+        setTitle(value)
+        break
+      case 'author':
+        setAuthor(value)
+        break
+      case 'intro':
+        setIntro(value)
+        break
+      case 'completed':
+        setCompleted(e.currentTarget.checked)
+        break
+      case 'review':
+        setReview(value)
+        break
+    }
+  }
+
+  const handleCover = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const element = e.currentTarget
+    if (element.files) {
+      const file = element.files[0]
+
+      const reader = new FileReader()
+      reader.readAsDataURL(file)
+      reader.onloadend = () => {
+        if (reader.result) {
+          setCover(reader.result.toString())
+        }
+      }
+    }
+  }
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    const newBook:NewBookInfo = {
+      id: crypto.randomUUID(),
+      title,
+      author,
+      cover,
+      intro,
+      completed,
+      review
+    }
+    console.log(newBook)
+  }
   return (
-    <div>NewBook</div>
+    <div>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <div>Title</div>
+          <input type="text"
+          name='title'
+          onChange={handleChange}
+          value={title}
+          />
+
+        </div>
+        <div>
+          <div>Author</div>
+          <input type="text"
+          name='author'
+          onChange={handleChange}
+          value={author}
+          />
+
+        </div>
+        <div>
+          <div>Cover</div>
+          <input type="file"
+          name='cover'
+          onChange={handleCover}
+          />
+          <div>
+            {cover && <img src={cover} alt="cover" width={200}/>}
+          </div>
+        </div>
+        <div>
+          <div>Introduction</div>
+          <input type="text"
+          name='intro'
+          onChange={handleChange}
+          value={intro}
+          />
+
+        </div>
+        <div>
+          <div>Completed</div>
+          <input type="checkbox"
+          name='completed'
+          onChange={handleChange}
+          // value={completed}
+          checked={completed}
+          />
+
+        </div>
+        <div>
+          <div>Review</div>
+          <input type="text"
+          name='review'
+          onChange={handleChange}
+          value={review}
+          />
+
+        </div>
+        <input type="submit" value="New Book" />
+      </form>
+    </div>
   )
 }
